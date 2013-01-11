a0bca1b8fb5cd5c6258800190f4af31cefd6a943
Milian Wolff <mail@milianw.de>
2013-01-11 19:31:12 +0100

    use proper version

--- version.h.cmake
+++ version.h.cmake
@@ -1,33 +1,16 @@
-/*****************************************************************************
- * This file is part of the KDE project
- * Copyright 2013 Milian Wolff <mail@milianw.de>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public License
- * along with this library; see the file COPYING.LIB.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301, USA.
- *******************************************************************************/
-#ifndef KDEVUPLOAD_VERSION_H
-#define KDEVUPLOAD_VERSION_H
+// THIS FILE IS GENERATED
+// WARNING! All changes made in this file will be lost!
+#ifndef VERSION_H
+#define VERSION_H
 
 #include <kdeversion.h>
 
-#define KDEVUPLOAD_MAJOR_VERSION @KDEVUPLOAD_VERSION_MAJOR@
-#define KDEVUPLOAD_MINOR_VERSION @KDEVUPLOAD_VERSION_MINOR@
-#define KDEVUPLOAD_PATCH_VERSION @KDEVUPLOAD_VERSION_PATCH@
+#define MAJOR_VERSION @VERSION_MAJOR@
+#define MINOR_VERSION @VERSION_MINOR@
+#define PATCH_VERSION @VERSION_PATCH@
 
-#define KDEVUPLOAD_VERSION_STR "@KDEVUPLOAD_VERSION_MAJOR@.@KDEVUPLOAD_VERSION_MINOR@.@KDEVUPLOAD_VERSION_PATCH@"
+#define VERSION_STR "@VERSION_MAJOR@.@VERSION_MINOR@.@VERSION_PATCH@"
 
-#define KDEVUPLOAD_VERSION KDE_MAKE_VERSION(@KDEVUPLOAD_VERSION_MAJOR@, @KDEVUPLOAD_VERSION_MINOR@, @KDEVUPLOAD_VERSION_PATCH@)
+#define VERSION KDE_MAKE_VERSION(@VERSION_MAJOR@, @VERSION_MINOR@, @VERSION_PATCH@)
 
 #endif
