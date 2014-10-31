37b8c4a63e4dbe1fb6caa4ec8de10a8f55d3fd9f
Jakub Caban <kuba@whyblack.pl>
2014-10-31 19:22:22 +0100

    Fix selecting large directories for uploading
    REVIEW: 120927

--- uploadprojectmodel.cpp
+++ uploadprojectmodel.cpp
@@ -136,12 +136,7 @@ bool UploadProjectModel::setData ( const QModelIndex & indx, const QVariant & va
             Qt::CheckState s = static_cast<Qt::CheckState>(value.toInt());
             m_checkStates.insert(indx, s);
 
-            //all parent nodes might have changed (the folders)
-            QModelIndex parent = indx;
-            while (parent.isValid()) {
-                emit dataChanged(parent, parent);
-                parent = parent.parent();
-            }
+            emit dataChanged(indx, indx);
             return true;
         } else if (i->folder()) {
             if (!rowCount(indx)) {
@@ -157,12 +152,7 @@ bool UploadProjectModel::setData ( const QModelIndex & indx, const QVariant & va
                 }
             }
 
-            //this + all parent items might have changed (from PartiallyChecked)
-            QModelIndex parent = indx;
-            while (parent.isValid()) {
-                emit dataChanged(parent, parent);
-                parent = parent.parent();
-            }
+            emit dataChanged(indx, indx);
             return true;
         }
     }
